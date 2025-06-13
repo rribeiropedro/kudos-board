980fc268efcb9337d71040c0cb3388a1d7b2bbb2
Pedro Rabadan Ribeiro <pedrorribeiro@meta.com>
2025-06-13 12:24:15 -0700

    created bord compoment and its layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Header from "./components/Header"
 import Features from "./components/Features"
+import Board from "./components/Board"
 import './styles/app.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header />
       <Features />
+      <Board />
     </>
   )
 }
---
+++ client/src/components/Board.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import '../styles/board.css'
+
+const Board = () => {
+  return (
+    <div className="board-container">
+      <div className="board-content">
+        <div className="board-title">
+          <h1>Boards</h1>
+          <hr />
+        </div>
+        <div className="board-grid">
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Board
